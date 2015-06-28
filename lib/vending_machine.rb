30bb6aba0381296efed11b426bf53191b2ed9a78
gabe <GabeMaker@users.noreply.github.com>
2015-06-28 21:32:49 +0100

    add #insert and @display

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -1,3 +1,9 @@
 class VendingMachine
 
+  attr_reader :display
+
+  def insert(coin)
+    @display = '5 cents'
+  end
+
 end
--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -1,5 +1,9 @@
 require 'vending_machine'
 
 describe VendingMachine do
+  it 'accepts coins and updates display' do
+    subject.insert('5')
+    expect(subject.display).to eq('5 cents')
+  end
 
 end
