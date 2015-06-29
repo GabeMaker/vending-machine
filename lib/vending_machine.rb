6ebd6f5fe677e1301c774a6f1125b402392444ca
gabe <GabeMaker@users.noreply.github.com>
2015-06-29 17:08:26 +0100

    display 'insert coin' when no coin inserted

--- lib/vending_machine.rb
+++ lib/vending_machine.rb
@@ -2,6 +2,10 @@ class VendingMachine
 
   attr_reader :display
 
+  def initialize
+    @display = 'INSERT COIN'
+  end
+
   def insert(coin)
     @display = '5 cents'
   end
--- spec/vending_machine_spec.rb
+++ spec/vending_machine_spec.rb
@@ -1,9 +1,12 @@
 require 'vending_machine'
 
 describe VendingMachine do
+  it "displays 'INSERT COIN' when no coin inserted" do
+    expect(subject.display).to eq('INSERT COIN')
+  end
+
   it 'accepts coins and updates display' do
     subject.insert('5')
     expect(subject.display).to eq('5 cents')
   end
-
 end
